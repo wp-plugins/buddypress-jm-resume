525cf3ece64526040f0b3db8d0281f3662a44a1a
kishores <kishores@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-18 09:52:55 +0000

    -- added resume listing and bug fix
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-jm-resume/trunk@1070298 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- buddypress-components/jm-resume-componet/bp-jm-resume/bp-jm-resume-screens.php
+++ buddypress-components/jm-resume-componet/bp-jm-resume/bp-jm-resume-screens.php
@@ -24,12 +24,35 @@ function bp_jm_screen() {
 }
 add_action( 'bp_screens', 'bp_jm_screen' );
 
-function bp_jm_resumes() { 
-	add_action('bp_template_content','load_projects_template');
+function bp_jm_screen_my_resumes() { 
+	add_action('bp_template_content','load_bp_jm_screen_template');
 }
 
-function load_projects_template() {
+function load_bp_jm_screen_template() {
     include  BUDDYPRESS_JM_RESUME.'/templates/jm-resume.php';
+}
+
+
+/**
+ * Show the resumes Job Manager template
+ *
+ * @since Job Manager (1.0.0)
+ */
+function bp_jm_screen_resumes() {
+
+	if ( bp_action_variables() ) {
+		bp_do_404();
+		return;
+	}
+
+	// Load the template
+	bp_core_load_template( apply_filters( 'bp_jm_screen_resumes', 'members/single/home' ) );
+	add_action('bp_template_content','load_jobs_resumes_template');
+}
+
+function load_bp_jm_screen_resumes_template() {
+    // include  file or echo do_shortcode
+    echo do_shortcode( '[resumes]' );
 }
 
 ?>
